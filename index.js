0c2ef4a162d504e7bb33ea887d332a3a2860e720
Samrat2814 <aslamhossainsamrat@gmail.com>
2025-03-09 07:40:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eqgf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Reviews data base
+    const reviewCollection = client.db("reviewsDB").collection("reviews");
+  
+    app.post("/reviews", async (req, res) => {
+        const newReview = req.body;
+        console.log(newReview);
+        const result = await reviewCollection.insertOne(newReview);
+        res.send(result);
+    })
+
+    app.get('/reviews', async (req, res) => {
+      const allReviews = await reviewCollection.find().toArray();
+      res.send(allReviews);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running ${port}`);
+});
